060d530f3bd988055659c18ecf6ceb89c0769c39
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2022-10-06 17:59:56 +0000

    Added num_guess.py

---
+++ num_guess.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created By: Jessah
+# Date: Oct 6, 2022
+# This program makes the user guess the number and outcome will vary.
+
+
+def main():
+    # input
+    print()
+    num = int(input("Enter the a number from 0 to 9: "))
+
+    # if statement for if the number is right or wrong number
+
+    if num == 3:
+        print("That's correct ;}")
+
+    else:
+        print("That's not the number ;{")
+
+
+if __name__ == "__main__":
+    main()
